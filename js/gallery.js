d4ac4806e4068f0e0718a44a57168b03cca5401d
AFSpeirs <andy.speirs2@gmail.com>
2017-02-20 22:14:56 +0000

    Made gallery page more dynamic

--- js/gallery.js
+++ js/gallery.js
@@ -1,15 +1,23 @@
 window.onload = function() { 
     const imgdiv = document.querySelector('#gallery');
     for (var i = 1; i <= 4; i++) {
-        imgdiv.innerHTML += '<img src="../img/gallery/img_' + i + '.jpg"></img>';			
+        imgdiv.innerHTML += '<a class="img-wrap" href="../img/gallery/large/img_' + i + '.jpg"><img src="../img/gallery/img_' + i + '.jpg"></img></a>';
     }
-
-    const imgtest = document.querySelector('img');
-
-    imgtest.addEventListener('click', function(e) {
-        console.log(this);
-        imgtest.style.width = this.naturalWidth /3;
-        // .add('width', this.naturalWidth / 3);
-        console.log(imgtest.naturalWidth);
-    });
 };
+
+window.onresize = function(e) {
+    const imgwrap = document.querySelector('img').naturalWidth;
+    const imgwrapArray = Array.from(document.getElementsByClassName("img-wrap"));
+
+    if(imgwrap > window.innerWidth) {
+        let imgwidth = document.querySelector('img').getBoundingClientRect().width;
+
+        imgwrapArray.forEach(function(element) {
+            element.style.height = imgwidth * 0.75 + 'px';
+        });
+    } else {
+        imgwrapArray.forEach(function(element) {
+            element.style.height = 'initial';
+        });
+    }
+}
