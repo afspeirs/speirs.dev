87809b2273cd259c4ebfd7c245e23e74011274db
Andy <andy.speirs2@gmail.com>
2017-02-21 22:45:42 +0000

    Fixed the regex in projects.js
    Now array is a standard for all functions to use

--- js/projects.js
+++ js/projects.js
@@ -1,33 +1,34 @@
 const screen = document.querySelector('img[data-screen]');
-const watch = document.querySelectorAll('.watch');
+const watch = Array.from(document.querySelectorAll('.watch'));
+let array = screen.src.replace(/\/([^\/]*)$/,'\/,'+'$1').replace(/_/g, ",_,").replace(/\.([^\.]*)$/, ',\.'+'$1').split(",");
 
-screen.addEventListener('click', function(e) {
-    let array = this.src.replace(/_/g, "_,").replace(/\./g, ",.").split(",");
-
-    if (this.classList.contains('rect') && array[1] == 5 ||
-        this.classList.contains('round') && array[1] == 4) {
-        array[1] = 1;
+function incrementScreenshot(e) {
+    if (this.classList.contains('rect') && array[3] == 5 ||
+        this.classList.contains('round') && array[3] == 4) {
+        array[3] = 1;
     } else {
-        array[1]++;
+        array[3]++;
     }
 
-    array = array.join('');
-    this.setAttribute("src", array)
-});
+    this.setAttribute("src", array.join(''));
+}
 
-Array.from(watch).forEach(button => {
-    button.addEventListener('click', function(e) {
-        let array = screen.src.replace(/\/([^\/]*)$/,'\/,'+'$1').replace(/_/g, ",").split(",");
+function swapWatch(e) {    
+    let array = screen.src.replace(/\/([^\/]*)$/,'\/,'+'$1').replace(/_/g, ",_,").replace(/\.([^\.]*)$/, ',\.'+'$1').split(",");
 
-        array[1] = this.dataset.watch + "_1"
-        array[2] = array[2].slice(-4);
-        array = array.join('');
-        screen.setAttribute("src", array);
+    array[1] = this.dataset.watch;
+    array[3] = 1;
 
-        if(this.dataset.screen != screen.classList) {
-            screen.classList.remove(screen.classList);
-            screen.classList.add(this.dataset.screen);
-            // console.log(this.dataset.screen);
-        }
-    });
+    screen.setAttribute("src", array.join(''));
+
+    if(this.dataset.screen != screen.classList) {
+        screen.classList.remove(screen.classList);
+        screen.classList.add(this.dataset.screen);
+    }
+}
+
+screen.addEventListener('click', incrementScreenshot);
+
+watch.forEach(button => {
+    button.addEventListener('click', swapWatch);
 });
