e75f7514122557ece0a7d6224634740afc058faf
Andy <andy.speirs2@gmail.com>
2017-02-21 21:07:54 +0000

    Changed js to work onload of window

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,10 @@
-const menubtn = document.querySelector('#menu-btn');
-const navmenu = document.querySelector('.nav-menu');
+window.onload = function() { 
+    const menubtn = document.querySelector('#menu-btn');
+    const navmenu = document.querySelector('.nav-menu');
 
-// Toggle Mobile navigation menu on click
-menubtn.addEventListener('click', function(e) {
-    navmenu.classList.toggle('expand');
-    menubtn.classList.toggle('open');
-});
+    // Toggle Mobile navigation menu on click
+    menubtn.addEventListener('click', function(e) {
+        navmenu.classList.toggle('expand');
+        menubtn.classList.toggle('open');
+    });
+};
