ad2d8eaacef6b455e0e7ad20448db5751d6621ce
Andy <andy.speirs2@gmail.com>
2017-02-21 20:35:27 +0000

    Added animation to #menu-btn

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,8 @@
 const menubtn = document.querySelector('#menu-btn');
 const navmenu = document.querySelector('.nav-menu');
 
+// Toggle Mobile navigation menu on click
 menubtn.addEventListener('click', function(e) {
     navmenu.classList.toggle('expand');
+    menubtn.classList.toggle('open');
 });
