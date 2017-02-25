a7a4afca94c3cc41b3561d7bb2b71e8ef9be9729
Andy <andy.speirs2@gmail.com>
2017-02-25 16:28:37 +0000

    Refined gulpfile and packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,15 @@
 var gulp = require('gulp'),
+    concat = require('gulp-concat'),
     handlebar = require('gulp-handlebars-all'),
-    rename = require('gulp-rename');
-    uglify = require('gulp-uglify');
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    del = require("del");
 
 // Default task
 // Compiles website 
-gulp.task('default', ['handlebar', 'scripts', 'styles', 'img']);
+gulp.task('default', ['handlebar', 'styles', 'scripts', 'img']);
 
-// Exports scripts to build folder
-gulp.task('scripts', function () {
-   	gulp.src('src/scripts/*.js')
-    	.pipe(gulp.dest('build/scripts'));
-});
-
-//Exports styles to the build folder
-gulp.task('styles', function () {
-   	gulp.src('src/styles/*.css')
-    	.pipe(gulp.dest('build/styles'));
-});
-
-//Exports styles to the build folder
-gulp.task('img', function () {
-	gulp.src('src/img/**/*')
-		.pipe(gulp.dest('build/img'));
-});
-
+// Exports html files to the build folder
 gulp.task('handlebar', function() {
 	gulp.src('src/*.hbs')
 		.pipe(handlebar('html', {
@@ -35,6 +20,29 @@ gulp.task('handlebar', function() {
 			path.extname = ".html";
 		}))
 		.pipe(gulp.dest('build'));
+});
+
+//Exports styles to the build folder
+gulp.task('styles', function () {
+   	gulp.src('src/styles/*.css')
+    	.pipe(gulp.dest('build/styles'));
+});
+
+// Exports scripts to build folder
+gulp.task('scripts', function () {
+   	gulp.src('src/scripts/*.js')
+    	.pipe(gulp.dest('build/scripts'));
+});
+
+//Exports images to the build folder
+gulp.task('img', function () {
+	gulp.src('src/img/**/*')
+		.pipe(gulp.dest('build/img'));
+});
+
+
+gulp.task('clean', function() {
+	del(['build/**/*']);
 });
 
 // Include watch function
