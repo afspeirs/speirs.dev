9fbd0d18593ef13101bf0cfc830c2d74ed63795e
Andy <andy.speirs2@gmail.com>
2017-05-19 22:28:13 +0100

    Add left and right arrows to watchface images

--- src/assets/js/pages/projects.js
+++ src/assets/js/pages/projects.js
@@ -1,18 +1,44 @@
 window.addEventListener('load', function() {
 	const screen = document.querySelectorAll('img[data-screen]'),
+	      imgLeft = document.querySelectorAll('#img-left'),
+	      imgRight = document.querySelectorAll('#img-right'),
 	      watch = document.querySelectorAll('.watch'),
 	      flipButton = document.querySelectorAll('#flip-button');
 
-	function incrementScreenshot() {
-		var array = this.src.replace(/\/([^\/]*)$/,'\/,'+'$1').replace(/_/g, ',_,').replace(/\.([^\.]*)$/, ',\.'+'$1').split(',');
+	function decrementScreenshot() {
+		var img = this.parentNode.firstChild;
+		var array = img.src.replace(/\/([^\/]*)$/,'\/,'+'$1').replace(/_/g, ',_,').replace(/\.([^\.]*)$/, ',\.'+'$1').split(',');
 
-		array[3]++;
-		this.setAttribute('src', array.join(''));
-
-		this.onerror = function() {
-			array[3] = 1
-			this.setAttribute('src', array.join(''));
+		if(array[3] == 1) {
+			if(img.dataset.screen === "Aplite") {
+				array[3] = img.dataset.aplite;
+			}
+			if(img.dataset.screen === "Basalt") {
+				array[3] = img.dataset.basalt;
+			}
+			if(img.dataset.screen === "Chalk") {
+				array[3] = img.dataset.chalk;
+			}
+		} else {
+			array[3]--;
 		}
+
+		img.setAttribute('src', array.join(''));
+	}
+
+	function incrementScreenshot() {
+		var img = this.parentNode.firstChild;
+		var array = img.src.replace(/\/([^\/]*)$/,'\/,'+'$1').replace(/_/g, ',_,').replace(/\.([^\.]*)$/, ',\.'+'$1').split(',');
+
+		if((img.dataset.screen === "Aplite" && array[3] === img.dataset.aplite) ||
+		   (img.dataset.screen === "Basalt" && array[3] === img.dataset.basalt) ||
+		   (img.dataset.screen === "Chalk" && array[3] === img.dataset.chalk)) {
+			array[3] = 1
+		} else {
+			array[3]++;
+		}
+
+		img.setAttribute('src', array.join(''));
 	}
 
 	function swapWatch() {
@@ -22,6 +48,7 @@ window.addEventListener('load', function() {
 		array[1] = this.dataset.watch;
 		array[3] = 1;
 
+		img.dataset.screen = this.dataset.watch;
 		img.setAttribute('src', array.join(''));
 
 		if(img.dataset.screen != img.classList) {
@@ -31,7 +58,11 @@ window.addEventListener('load', function() {
 	}
 
 
-	screen.forEach(function(e) {
+	imgLeft.forEach(function(e) {
+		e.addEventListener('click', decrementScreenshot);
+	});
+
+	imgRight.forEach(function(e) {
 		e.addEventListener('click', incrementScreenshot);
 	});
 
