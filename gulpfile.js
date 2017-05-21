76101d4f78eb3fd7219abc841dc7a67ece84bc61
Andy <andy.speirs2@gmail.com>
2017-05-21 22:11:27 +0100

    Removed unecessary whitespace

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var gulp         = require('gulp'),                     // https://www.npmjs.com
     htmlmin      = require('gulp-htmlmin'),             // https://www.npmjs.com/package/gulp-htmlmin
     rename       = require('gulp-rename'),              // https://www.npmjs.com/package/gulp-rename
     sass         = require('gulp-sass'),                // https://www.npmjs.com/package/gulp-sass
-	uglify       = require('gulp-uglify'),              // https://www.npmjs.com/package/gulp-uglify
+    uglify       = require('gulp-uglify'),              // https://www.npmjs.com/package/gulp-uglify
     browserSync  = require('browser-sync').create(),    // https://www.npmjs.com/package/browser-sync
     del          = require("del");                      // https://www.npmjs.com/package/del
 
