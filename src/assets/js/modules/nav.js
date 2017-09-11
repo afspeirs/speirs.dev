9bef83e0301039e7ccdb9d576428c9495761e98c
Andy Speirs <Andy.Speirs@havasww.com>
2017-09-11 14:02:08 +0100

    Used arrow functions

--- src/assets/js/modules/nav.js
+++ src/assets/js/modules/nav.js
@@ -1,9 +1,9 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', () => {
 	const menubtn = document.querySelector('#menu-btn');
 	const navmenu = document.querySelector('.nav-menu');
 
 	// Toggle Mobile navigation menu on click
-	menubtn.addEventListener('click', function() {
+	menubtn.addEventListener('click', () => {
 		menubtn.classList.toggle('open');
 		navmenu.classList.toggle('expand');
 	});
