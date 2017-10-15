e55183181fb90a36ec600ba41c198fb6475b61a7
Andy <andy.speirs2@gmail.com>
2017-10-15 17:19:02 +0100

    Reworked the navigation bar to include a seperate mobile navigation (to fix issues with resizing the window)

--- src/assets/js/modules/nav.js
+++ src/assets/js/modules/nav.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', () => {
 	const menubtn = document.querySelector('#menu-btn');
-	const navmenu = document.querySelector('.nav-menu');
+	const navmenu = document.querySelector('.mobile-nav-menu');
 
 	// Toggle Mobile navigation menu on click
 	menubtn.addEventListener('click', () => {
