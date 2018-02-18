1e629500eff795334b704c47245556490513fa57
ANDYLAPTOP\Andy <andy.speirs2@gmail.com>
2018-02-18 22:44:08 +0000

    Rotate logo on hover, randomised on page load

--- src/assets/js/modules/nav.js
+++ src/assets/js/modules/nav.js
@@ -2,16 +2,11 @@ window.addEventListener('load', () => {
 	const menubtn = document.querySelector('#menu-btn');
 	const navmenu = document.querySelector('.mobile-nav-menu');
 
-	const container = document.querySelector('#container')
+	const container = document.querySelector('#container');
 	const content = [...document.querySelectorAll('.content')];
-	const navA = document.querySelectorAll('#navbar-wrap a')
+	const navA = document.querySelectorAll('#navbar-wrap a');
 
-	// Toggle Mobile navigation menu on click
-	menubtn.addEventListener('click', () => {
-		menubtn.classList.toggle('open');
-		navmenu.classList.toggle('expand');
-	});
-
+	// A debouce function to limit the number of times a function is run
 	function debounce(func, wait, immediate) {
 		var timeout;
 		return function() {
@@ -27,6 +22,7 @@ window.addEventListener('load', () => {
 		};
 	};
 
+	// Sets the current section active
 	function activeNavSection(compare) {
 		navA.forEach(a => {
 			const id = a.href.substr(a.href.lastIndexOf('#') + 1);
@@ -39,26 +35,32 @@ window.addEventListener('load', () => {
 		});
 	}
 
-	// Run on page load to show which section the user is at
-	activeNavSection(window.location.hash.substr(1))
-
-	// navA.forEach(e => e.addEventListener('click', function() {
-	// 	// navA.forEach(a => a.classList.remove('active'));
-	// 	// e.classList.add('active');
-	// 	activeNavSection();
-	// }))
-
+	// Based on scroll position set the current section as active
 	function scrollMe() {
 		const y = container.scrollTop + 1;
 
 		for (let i = 0; i < content.length; i++) {
 			if ((i !== content.length - 1 && content[i].offsetTop < y && content[i + 1].offsetTop > y) ||
-			    (i === content.length - 1 && content[i].offsetTop < y)) {
+			(i === content.length - 1 && content[i].offsetTop < y)) {
 				activeNavSection(content[i].id)
 			};
 		}
 	}
 
+	// Randomly tilt logo
+	const tileAngle = 25;
+	document.documentElement.style.setProperty(`--logo-tilt-degree`, `${Math.floor(Math.random() * (tileAngle - -tileAngle)) + -tileAngle}deg`);
+
+	// Toggle Mobile navigation menu on click
+	menubtn.addEventListener('click', () => {
+		menubtn.classList.toggle('open');
+		navmenu.classList.toggle('expand');
+	});
+
+	// Run on page load to show which section the user is at
+	activeNavSection(window.location.hash.substr(1))
+
+	// Check for scroll event and add active to navigation
 	container.addEventListener('scroll', debounce(scrollMe));
 });
 
