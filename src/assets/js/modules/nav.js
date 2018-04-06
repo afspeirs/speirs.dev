69dc82557b34951b13fa96a06f4f1cacb0a9b260
Andy Speirs <Andy.Speirs@havas.com>
2018-04-06 09:06:36 +0100

    Updated Nav module

--- src/assets/js/modules/nav.js
+++ src/assets/js/modules/nav.js
@@ -1,70 +1,70 @@
-var Nav = {
+const Nav = {
+	menubtn: document.querySelector('#menu-btn'),
+	navmenu: document.querySelector('.mobile-nav-menu'),
+	container: document.querySelector('#container'),
+	content: [...document.querySelectorAll('.content')],
+	navA: document.querySelectorAll('#navbar-wrap a'),
+
 	init: function() {
-		const menubtn = document.querySelector('#menu-btn');
-		const navmenu = document.querySelector('.mobile-nav-menu');
+		// Toggle Mobile navigation menu on click
+		Nav.menubtn.addEventListener('click', () => {
+			Nav.menubtn.classList.toggle('open');
+			Nav.navmenu.classList.toggle('expand');
+		});
 
-		const container = document.querySelector('#container');
-		const content = [...document.querySelectorAll('.content')];
-		const navA = document.querySelectorAll('#navbar-wrap a');
+		Nav.tiltLogo();
 
-		// A debouce function to limit the number of times a function is run
-		function debounce(func, wait, immediate) {
-			var timeout;
-			return function() {
-				var context = this, args = arguments;
-				var later = function() {
-					timeout = null;
-					if (!immediate) func.apply(context, args);
-				};
-				var callNow = immediate && !timeout;
-				clearTimeout(timeout);
-				timeout = setTimeout(later, wait);
-				if (callNow) func.apply(context, args);
+		// Run on page load to show which section the user is at
+		Nav.activeNavSection(window.location.hash.substr(1) || Nav.content[0].id);
+
+		// Check for scroll event and add active to navigation
+		document.addEventListener('scroll', Nav.debounce(Nav.onScroll));
+	},
+	// A debouce function to limit the number of times a function is run
+	debounce: function(func, wait, immediate) {
+		var timeout;
+		return function() {
+			var context = this, args = arguments;
+			var later = function() {
+				timeout = null;
+				if (!immediate) func.apply(context, args);
 			};
-		}
-
-		// Sets the current section active
-		function activeNavSection(compare) {
-			navA.forEach(a => {
-				const id = a.href.substr(a.href.lastIndexOf('#') + 1);
+			var callNow = immediate && !timeout;
+			clearTimeout(timeout);
+			timeout = setTimeout(later, wait);
+			if (callNow) func.apply(context, args);
+		};
+	},
+	// Sets the current section active
+	activeNavSection: function(compare) {
+		Nav.navA.forEach(a => {
+			const id = a.href.substr(a.href.lastIndexOf('#') + 1);
+			// console.log(id);
+			a.classList.remove('active');
+			if (compare === id) {
 				// console.log(id);
-				a.classList.remove('active');
-				if (compare === id) {
-					// console.log(id);
-					a.classList.add('active');
-				}
-			});
-		}
+				a.classList.add('active');
+			}
+		});
+	},
+	// Based on scroll position set the current section as active
+	onScroll: function() {
+		// const y = container.scrollTop + 1;
+		const y = window.scrollY + 1;
 
-		// Based on scroll position set the current section as active
-		function onScroll() {
-			// const y = container.scrollTop + 1;
-			const y = window.scrollY + 1;
-
-			for (let i = 0; i < content.length; i++) {
-				if ((i !== content.length - 1 && content[i].offsetTop < y && content[i + 1].offsetTop > y) ||
-					(i === content.length - 1 && content[i].offsetTop < y)) {
-					activeNavSection(content[i].id);
-				}
+		for (let i = 0; i < Nav.content.length; i++) {
+			if ((i !== Nav.content.length - 1 && Nav.content[i].offsetTop < y && Nav.content[i + 1].offsetTop > y) ||
+				(i === Nav.content.length - 1 && Nav.content[i].offsetTop < y)) {
+				Nav.activeNavSection(Nav.content[i].id);
 			}
 		}
-
+	},
+	// Set a random title angle for the logo
+	tiltLogo: function(params) {
 		// Randomly tilt logo
 		const tileAngle = 25;
 		document.documentElement.style.setProperty(`--logo-tilt-degree`, `${Math.floor(Math.random() * (tileAngle - -tileAngle)) + -tileAngle}deg`);
-
-		// Toggle Mobile navigation menu on click
-		menubtn.addEventListener('click', () => {
-			menubtn.classList.toggle('open');
-			navmenu.classList.toggle('expand');
-		});
-
-		// Run on page load to show which section the user is at
-		activeNavSection(window.location.hash.substr(1) || content[0].id);
-
-		// Check for scroll event and add active to navigation
-		document.addEventListener('scroll', debounce(onScroll));
-	}
+	},
 };
 
 export default Nav;
