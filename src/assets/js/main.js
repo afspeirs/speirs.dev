b457169028897712e0f3e6a9940640ab94b4ee3c
andy <andy.speirs2@gmail.com>
2018-04-07 21:02:15 +0100

    Lazy load images by changing data-src to src Added noscript fallback Added jsonly class

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,6 +3,14 @@ import Screenshot from './modules/screenshot';
 import Watchface from './modules/watchface';
 
 window.addEventListener('load', function() {
+	// Lazy load images
+	[].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
+		img.setAttribute('src', img.getAttribute('data-src'));
+		img.onload = function() {
+			img.removeAttribute('data-src');
+		};
+	});
+
 	Nav.init();
 	Screenshot.init();
 	Watchface.init();
