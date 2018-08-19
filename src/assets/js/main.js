5fd9b84d4f3170b41eb3e1ac715f1087b65759ea
andy <andy.speirs2@gmail.com>
2018-08-19 13:02:50 +0100

    Add function to incement the colour of a css variable

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -16,10 +16,27 @@ window.addEventListener('load', function () {
 	const tileAngle = 25;
 	document.documentElement.style.setProperty(`--logo-tilt-degree`, `${Math.floor(Math.random() * (tileAngle - -tileAngle)) + -tileAngle}deg`);
 
+	// Module Init
 	Nav.init();
 	Screenshot.init();
 	Tab.init();
 	Watchface.init();
+
+	// // Change colour
+	// var start = 0x000000;
+	// var end = 0xFFFFFF;
+	// var value;
+	// var intervalFillColour = setInterval(function(){
+	// 	if (start === end) {
+	// 		clearInterval(intervalFillColour)
+	// 	}
+	// 	value = start.toString(16);
+	// 	if (value.length < 8) {
+	// 		value = "000000".substring(0, 6 - value.length) + value;
+	// 	}
+	// 	start++;
+	// 	document.documentElement.style.setProperty('--svg-fill-colour', `#${value}`);
+	// }, 400);
 
 	// const scrollToTop = _ => {
 	// 	const c = document.documentElement.scrollTop || document.body.scrollTop;
