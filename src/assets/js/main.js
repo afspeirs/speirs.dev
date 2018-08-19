47bd064d0b11a5f94e32ac4edddd00eb03fdea2f
andy <andy.speirs2@gmail.com>
2018-08-19 13:46:42 +0100

    Updated arrow in home section to scroll to projects section

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -37,13 +37,4 @@ window.addEventListener('load', function () {
 	// 	start++;
 	// 	document.documentElement.style.setProperty('--svg-fill-colour', `#${value}`);
 	// }, 400);
-
-	// const scrollToTop = _ => {
-	// 	const c = document.documentElement.scrollTop || document.body.scrollTop;
-	// 	if (c > 0) {
-	// 		window.requestAnimationFrame(scrollToTop);
-	// 		window.scrollTo(0, c - c / 8);
-	// 	}
-	// }
-	// scrollToTop();
 });
