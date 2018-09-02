6c75bd7534dd6c49f2e19fbabea4b3241fba4521
andy <andy.speirs2@gmail.com>
2018-09-02 15:29:20 +0100

    Removed lazy load of images

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,24 +5,16 @@ import Watchface from './modules/watchface';
 import Tab from './modules/tab';
 
 window.addEventListener('load', () => {
-	// Lazy load images
-	[].forEach.call(document.querySelectorAll('img[data-src]'), (image) => {
-		image.setAttribute('src', image.getAttribute('data-src'));
-		image.onload = () => {
-			image.removeAttribute('data-src');
-		};
-	});
-
-	// Set a random title angle for the logo
-	const tileAngle = 25;
-	document.documentElement.style.setProperty('--logo-tilt-degree', `${Math.floor(Math.random() * (tileAngle - -tileAngle)) + -tileAngle}deg`);
-
 	// Module Init
 	Nav.init();
 	Gallery.init();
 	Screenshot.init();
 	Tab.init();
 	Watchface.init();
+
+	// Set a random title angle for the logo
+	const tileAngle = 25;
+	document.documentElement.style.setProperty('--logo-tilt-degree', `${Math.floor(Math.random() * (tileAngle - -tileAngle)) + -tileAngle}deg`);
 
 	// // Change colour
 	// var start = 0x000000;
