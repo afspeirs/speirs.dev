4bc52c9154abc5d6d40039e340d21e61a207ec76
andy <andy.speirs2@gmail.com>
2018-09-02 11:25:08 +0100

    Refactored gallery module code

--- src/assets/js/modules/gallery.js
+++ src/assets/js/modules/gallery.js
@@ -15,27 +15,26 @@ const Gallery = {
 		Gallery.imgMain.alt = this.dataset.name;
 		Gallery.imgMain.dataset.index = this.dataset.index;
 	},
+	updateImageFromIndex(currentIndex) {
+		const newImage = Gallery.imgSecondary
+			.filter(image => parseInt(image.dataset.index, 10) === currentIndex + 1)[0];
+		Gallery.imgMain.src = newImage.src;
+		Gallery.imgMain.alt = newImage.dataset.name;
+		Gallery.imgMain.dataset.index = newImage.dataset.index;
+	},
 	prevImage() {
 		let currentIndex = parseInt(Gallery.imgMain.dataset.index, 10);
 		if (currentIndex === 0) {
 			currentIndex = Gallery.imgSecondary.length;
 		}
-		const newImage = Gallery.imgSecondary
-			.filter(image => parseInt(image.dataset.index, 10) === currentIndex - 1)[0];
-		Gallery.imgMain.src = newImage.src;
-		Gallery.imgMain.alt = newImage.dataset.name;
-		Gallery.imgMain.dataset.index = newImage.dataset.index;
+		Gallery.updateImageFromIndex(currentIndex);
 	},
 	nextImage() {
 		let currentIndex = parseInt(Gallery.imgMain.dataset.index, 10);
 		if (currentIndex === Gallery.imgSecondary.length - 1) {
 			currentIndex = -1;
 		}
-		const newImage = Gallery.imgSecondary
-			.filter(image => parseInt(image.dataset.index, 10) === currentIndex + 1)[0];
-		Gallery.imgMain.src = newImage.src;
-		Gallery.imgMain.alt = newImage.dataset.name;
-		Gallery.imgMain.dataset.index = newImage.dataset.index;
+		Gallery.updateImageFromIndex(currentIndex);
 	},
 };
 
