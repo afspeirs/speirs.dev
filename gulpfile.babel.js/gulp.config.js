15d412cb2d2237adc6c193b662f5fce6dec305be
Andy <andy.speirs2@gmail.com>
2019-01-27 12:13:57 +0000

    Created settings for browserSync

--- gulpfile.babel.js/gulp.config.js
+++ gulpfile.babel.js/gulp.config.js
@@ -11,3 +11,15 @@ export const paths = {
 	partials: 'templates/partials/',
 	sections: 'templates/sections/',
 };
+
+export const settings = {
+	index: 'index.html',
+	// Clicks, Scrolls & Form inputs on any device will be mirrored to all others.
+	ghostMode: false,
+	// Force HTTPS
+	https: false,
+	// Change the port from the default
+	port: 3000,
+	// Wait 2 seconds after a reload event before allowing more.
+	reloadDebounce: 2000,
+};
--- gulpfile.babel.js/tasks/server.js
+++ gulpfile.babel.js/tasks/server.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+
 import { series, watch } from 'gulp';
 import browserSync from 'browser-sync';
 
@@ -7,7 +9,7 @@ import { jsClean, jsFiles } from './js';
 import { pagesClean, pagesFiles } from './pages';
 import { rootClean, rootFiles } from './root';
 
-import { paths } from '../gulp.config';
+import { paths, settings } from '../gulp.config';
 
 const css = series(cssClean, cssFiles);
 const img = series(imgClean, imgFiles);
@@ -20,30 +22,25 @@ export const server = () => {
 	browserSync.init({
 		server: {
 			baseDir: paths.build,
-			index: 'index.html',
+			index: settings.index,
 		},
 		// Clicks, Scrolls & Form inputs on any device will be mirrored to all others.
-		ghostMode: false,
+		ghostMode: settings.ghostMode,
+		// Force HTTPS
+		https: settings.https,
 		// Don't show any notifications in the browser.
 		notify: false,
 		// Change the port from the default
-		port: 3000,
+		port: settings.port,
 		// Wait 2 seconds after a reload event before allowing more.
-		reloadDebounce: 2000,
+		reloadDebounce: settings.reloadDebounce,
 	});
 
-	watch(`${paths.src + paths.img}**/*`, img)
-		.on('change', browserSync.reload);
-	watch(`${paths.src + paths.js}**/*`, js)
-		.on('change', browserSync.reload);
-	watch(`${paths.src + paths.css}**/*`, css)
-		.on('change', browserSync.reload);
-	watch(`${paths.src + paths.data}**/*`, pages)
-		.on('change', browserSync.reload);
-	watch(`${paths.src}templates/**/*.hbs`, pages)
-		.on('change', browserSync.reload);
-	watch(`${paths.src}*.*`, root)
-		.on('change', browserSync.reload);
-	watch(`${paths.helpers}**/*.js`, pages)
-		.on('change', browserSync.reload);
+	watch(`${paths.src + paths.img}**/*`, img).on('change', browserSync.reload);
+	watch(`${paths.src + paths.js}**/*`, js).on('change', browserSync.reload);
+	watch(`${paths.src + paths.css}**/*`, css).on('change', browserSync.reload);
+	watch(`${paths.src + paths.data}**/*`, pages).on('change', browserSync.reload);
+	watch(`${paths.src}templates/**/*.hbs`, pages).on('change', browserSync.reload);
+	watch(`${paths.src}*.*`, root).on('change', browserSync.reload);
+	watch(`${paths.helpers}**/*.js`, pages).on('change', browserSync.reload);
 };
