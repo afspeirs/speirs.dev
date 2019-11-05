6ce83ab28ceea40680327500fe194d39ea889a6d
Andy Speirs <andy.speirs2@gmail.com>
2019-11-05 22:06:39 +0000

    Add dark theme toggle (#5)
    * Add dark theme toggle
    
    Uses css variables to change to dark theme
    
    * Created DarkTheme module

---
+++ src/assets/js/modules/dark-theme.js
@@ -0,0 +1,35 @@
+const DarkTheme = (function DarkTheme() {
+	let currentTheme = localStorage.getItem('theme') || null;
+	const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
+
+	function switchTheme(event) {
+		if (event.target.checked) {
+			document.documentElement.setAttribute('data-theme', 'dark');
+			localStorage.setItem('theme', 'dark');
+		} else {
+			document.documentElement.setAttribute('data-theme', '');
+			localStorage.removeItem('theme');
+		}
+	}
+
+	function init() {
+		if (!currentTheme && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+			currentTheme = 'dark';
+		}
+
+		if (currentTheme) {
+			document.documentElement.setAttribute('data-theme', currentTheme);
+			if (currentTheme === 'dark') {
+				toggleSwitch.checked = true;
+			}
+		}
+
+		toggleSwitch.addEventListener('change', switchTheme, false);
+	}
+
+	return {
+		init,
+	};
+}());
+
+export default DarkTheme;
