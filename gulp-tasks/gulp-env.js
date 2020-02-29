1156084235305d70bc851a4b8921d59365196efd
afspeirs <andy.speirs2@gmail.com>
2020-02-29 12:04:22 +0000

    Added  debug script
    You can also call it by passing --debug argument

--- gulp-tasks/gulp-env.js
+++ gulp-tasks/gulp-env.js
@@ -1,5 +1,11 @@
 export const setDev = (done) => {
 	process.env.NODE_ENV = 'dev';
+
+	// if --debug argument is passed. Enable debug mode
+	if (process.env.npm_config_debug) {
+		process.env.DEBUG = 'Eleventy*';
+	}
+
 	done();
 };
 export const setProd = (done) => {
