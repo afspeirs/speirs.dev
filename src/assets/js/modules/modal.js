e4c395ee96586cfda67c52c30d80d518eae1cc6b
Andy <andy.speirs2@gmail.com>
2020-02-29 20:22:58 +0000

    Updated modal module

--- src/assets/js/modules/modal.js
+++ src/assets/js/modules/modal.js
@@ -1,42 +1,46 @@
 export default (function Modal() {
 	const modalBackground = document.querySelector('#modal-background');
-	const modalFullscreenClose = document.querySelector('.modal.fullscreen-close');
+	const modalFullscreenClose = [...document.querySelectorAll('.modal.fullscreen-close')];
 	const modalClose = [...document.querySelectorAll('.modal-close')];
 	const dataModal = [...document.querySelectorAll('[data-modal]')];
 	const allModals = [...document.querySelectorAll('.modal')];
 	const openModals = [];
 
-	const getModalFromId = (idModal) => allModals.filter((modal) => modal.id === `modal-${idModal}`)[0];
+	const getModalFromId = (id) => allModals.find((modal) => modal.id === `modal-${id}`);
 
-	function openModal(idModal) {
-		const foundModal = getModalFromId(idModal);
+	function openModal(id) {
+		const foundModal = getModalFromId(id);
 
 		if (foundModal) {
 			foundModal.classList.add('active');
 			modalBackground.classList.add('active');
-			openModals.push(idModal);
+			openModals.push(id);
 		} else {
-			// eslint-disable-next-line no-console
-			console.error('modal not found');
+			console.error(`No modal called "${id}" was found`); // eslint-disable-line no-console
 		}
 	}
-	function closeModal() {
-		const latestModal = openModals[openModals.length - 1];
-		const foundModal = getModalFromId(latestModal);
-		foundModal.classList.remove('active');
-		modalBackground.classList.remove('active');
-		openModals.splice(openModals.length - 1, 1);
-	}
-	function keyPress(key) {
-		if (key.keyCode === 27) {
-			Modal.closeModal();
+
+	function closeModal(id = openModals[openModals.length - 1]) {
+		const foundModal = getModalFromId(id);
+
+		if (foundModal) {
+			foundModal.classList.remove('active');
+			modalBackground.classList.remove('active');
+			openModals.pop();
+		} else {
+			console.error(`No modal called "${id}" was found`); // eslint-disable-line no-console
 		}
 	}
+
+	function keyPress(event) {
+		if (event.key === 'Escape' && openModals.length !== 0) closeModal();
+	}
+
 	function init() {
 		if (dataModal) dataModal.forEach((button) => button.addEventListener('click', () => openModal(button.dataset.modal), false));
 		// Close modal and background on click of the modal background
 		if (modalBackground) modalBackground.addEventListener('click', closeModal, false);
-		if (modalFullscreenClose) modalFullscreenClose.addEventListener('click', closeModal, false);
+		if (modalFullscreenClose) modalFullscreenClose.forEach((modal) => modal.addEventListener('click', closeModal, false));
 		if (modalClose) modalClose.forEach((modal) => modal.addEventListener('click', closeModal, false));
 		// Close modal on keydown of esc key
 		document.addEventListener('keydown', keyPress, false);
