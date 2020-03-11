a099147e7efaadf7c3d8c0c2ef16602a28d1b332
Andy <andy.speirs2@gmail.com>
2020-03-11 21:25:48 +0000

    Remove all content to the site

--- src/assets/js/modules/debounce.js
+++
@@ -1,9 +0,0 @@
-// A debouce function to limit the number of times a function is run
-export default (func, wait) => {
-	let timeout;
-	return (...args) => {
-		const context = this;
-		clearTimeout(timeout);
-		timeout = setTimeout(() => func.apply(context, args), wait);
-	};
-};
