026c387b080bb3f7c414cf6ddf44434049fa08dd
Andy <andy.speirs2@gmail.com>
2020-03-27 10:42:27 +0000

    Have a no-js class on the html element by default
    It is removed if the js loads

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,8 @@
 import LogoTilt from './modules/logo-tilt';
 import Nav from './modules/nav';
 
+// Remove no-js class as JavaScript is being used
+document.documentElement.classList.remove('no-js');
+
 LogoTilt.init();
 Nav.init();
