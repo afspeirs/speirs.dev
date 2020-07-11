c4ab5077132352de1acd2173b32d6c4634229a03
Andy <andy.speirs2@gmail.com>
2020-07-11 21:22:07 +0100

    Refactor
    Reamove nav.js
    Restyled site
    Refactored site

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,6 @@
 import LogoTilt from './modules/logo-tilt';
-import Nav from './modules/nav';
 
 // Remove no-js class as JavaScript is being used
 document.documentElement.classList.remove('no-js');
 
 LogoTilt.init();
-Nav.init();
