ef7fbe65dd7b90bf533d460678f933e23f5e8a05
Andy <andy.speirs2@gmail.com>
2020-07-19 10:14:42 +0100

    Updated how the navigation is defined

---
+++ src/data/navigation.js
@@ -0,0 +1,18 @@
+module.exports = {
+	home: {
+		link: '/',
+		name: 'Home',
+	},
+	projects: {
+		link: '/projects/',
+		name: 'Projects',
+	},
+	other: {
+		link: '/other/',
+		name: 'Other',
+	},
+	about: {
+		link: '/about/',
+		name: 'About',
+	},
+};
