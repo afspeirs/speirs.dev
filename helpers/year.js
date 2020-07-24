347b78155cc4d8fa19514822039366fe0da478b8
Andy <andy.speirs2@gmail.com>
2020-07-24 22:24:02 +0100

    Created formatDate helper

--- helpers/year.js
+++
@@ -1,5 +0,0 @@
-// Returns the year of the date provided or the current year
-module.exports = function year(date) {
-	if (date) return new Date(date).getFullYear();
-	return new Date().getFullYear();
-};
