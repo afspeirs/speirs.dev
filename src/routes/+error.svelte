30e27bffdc69bdf8350f39ea0188e988b1ef4e80
Andy <andy@speirs.dev>
2023-01-02 19:50:19 +0000

    feat: add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { page } from '$app/stores';
+  import Card from '$lib/components/Card.svelte';
+</script>
+
+<svelte:head>
+  <title>{$page.status}: {$page.error?.message}</title>
+</svelte:head>
+
+<Card title={$page.status.toString()}>
+  <p>{$page.error?.message}</p>
+</Card>
