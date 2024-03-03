32e45061c1994d2d7dd64543ec0cb9ee1ee35854
andy.speirs <andy.speirs@havas.com>
2024-03-03 19:06:11 +0000

    feat: add macos icon

---
+++ src/lib/utils/webmanifest.ts
@@ -0,0 +1,23 @@
+export function getWebmanifest(manifest: string, apple = false) {
+  const webmanifest = JSON.parse(manifest);
+
+  const branchName = process?.env?.HEAD || 'local';
+  console.log(branchName);
+
+
+  if (branchName && branchName !== 'main') {
+    webmanifest.name += ` (${branchName})`;
+  }
+
+  if (apple) {
+    webmanifest.icons = [
+      {
+        src: '/icon-apple-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ]
+  }
+
+  return JSON.stringify(webmanifest, null, 2);
+}
---
+++ src/routes/manifest.webmanifest/+server.ts
@@ -0,0 +1,10 @@
+
+import { getWebmanifest } from '$lib/utils/webmanifest';
+import manifestRaw from './manifest.webmanifest?raw';
+
+export const prerender = true;
+
+export function GET() {
+  const newManifest = getWebmanifest(manifestRaw);
+  return new Response(newManifest);
+}
