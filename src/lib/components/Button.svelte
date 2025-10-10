b7f31bd00286f18089fd37e4cd83c70601876fb4
Andrew Speirs <andrew@speirs.dev>
2025-10-10 23:59:00 +0100

    feat: update buttons to be able to do external link refactor: update button to use svelte:component

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import type { Icon as IconType } from 'lucide-svelte';
-  import type { Snippet } from 'svelte';
+  import type { IconProps, Icon as IconType } from 'lucide-svelte';
+  import type { Component, Snippet } from 'svelte';
 
   import type { ResolvedPathname } from '$app/types';
   import { classNames } from '$lib/utils/classNames';
@@ -9,21 +9,22 @@
   interface ButtonBase {
     children?: Snippet;
     class?: string;
-    icon?: typeof IconType;
+    icon?: Component<IconProps> | typeof IconType;
+    iconProps?: IconProps;
     iconOnly?: boolean;
     invert?: boolean;
     text: string;
   }
 
   interface ButtonClickProps extends ButtonBase {
-    current?: never,
+    current?: never;
     href?: never;
     onclick: () => void;
   }
 
   interface ButtonLinkProps extends ButtonBase {
-    current?: AriaAttributes['aria-current'],
-    href: ResolvedPathname;
+    current?: AriaAttributes['aria-current'];
+    href: ResolvedPathname | `https://${string}`;
     onclick?: never;
   }
 
@@ -33,53 +34,47 @@
     current,
     href,
     icon: Icon,
+    iconProps = {
+      class: 'size-5',
+    },
     iconOnly = false,
     invert = false,
     onclick,
     text,
   }: ButtonClickProps | ButtonLinkProps = $props();
+
+  const tag = href ? 'a' : 'button';
+  const externalHref = href ? href?.startsWith('https://') : undefined;
+  const attrs = href
+    ? {
+      href,
+      'aria-current': current,
+      target: externalHref ? '_blank' : undefined,
+      rel: externalHref ? 'noopener noreferrer' : undefined,
+    }
+    : {
+      onclick,
+      type: 'button' as const,
+    };
+
+  const finalClass = classNames(
+    'p-2 rounded-md cursor-pointer focus-visible ring-inset focus-outline-inset-invert',
+    invert ? 'hover:bg-black/20' : 'hover:bg-tertiary hover:text-white dark:hover:text-dark-1',
+    iconOnly ? 'block' : 'flex items-center gap-2 text-sm text-dark-1 dark:text-white',
+    className,
+  );
 </script>
 
-{#if href}
-  <a
-    class={classNames(
-      'block',
-      'p-2 rounded-md cursor-pointer focus-visible ring-inset focus-outline-inset-invert',
-      invert ? 'hover:bg-black/20' : 'hover:bg-tertiary hover:text-white dark:hover:text-dark-1',
-      !iconOnly && 'flex items-center gap-2 text-sm text-dark-1 dark:text-white',
-      className,
-    )}
-    {href}
-    aria-current={current}
-  >
-    {#if Icon}
-      <Icon class="size-5" aria-hidden="true" />
-    {/if}
+<svelte:element this={tag} class={finalClass} {...attrs}>
+  {#if Icon}
+    <Icon {...iconProps} aria-hidden="true" />
+  {/if}
 
-    <span class={iconOnly ? 'sr-only' : undefined}>{text}</span>
+  <span class:sr-only={iconOnly}>
+    {text}
+  </span>
 
-    {#if children}
-      {@render children()}
-    {/if}
-  </a>
-{:else}
-  <button
-    class={classNames(
-      'p-2 text-sm rounded-md cursor-pointer focus-visible ring-inset focus-outline-inset-invert',
-      invert ? 'hover:bg-black/20' : 'hover:bg-primary hover:text-light-1',
-      !iconOnly && 'flex items-center gap-2 text-gray-900 dark:text-white',
-      className,
-    )}
-    {onclick}
-  >
-    {#if Icon}
-      <Icon class="size-5" aria-hidden="true" />
-    {/if}
-
-    <span class={iconOnly ? 'sr-only' : undefined}>{text}</span>
-
-    {#if children}
-      {@render children()}
-    {/if}
-  </button>
-{/if}
+  {#if children}
+    {@render children()}
+  {/if}
+</svelte:element>
