02f687915450940fa802bc869bbc5f7cfa23875f
Andrew Speirs <andrew@speirs.dev>
2025-10-15 21:33:15 +0100

    feat: update copy across the website feat: add the ability to sort the projects when getting them

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -3,11 +3,25 @@ import { toKebabCase } from '$lib/utils';
 
 type File = { metadata: Omit<Project, 'slug' | 'url'> };
 
-export async function getProjects() {
+type SortKey = 'date' | 'title';
+type SortOperator = '+' | '-';
+type SortByOption = `${SortOperator}${SortKey}`;
+
+type GetProjectsOptions = {
+  customFilter?: (project: Project) => boolean;
+  sortBy?: SortByOption;
+};
+
+export async function getProjects({
+  customFilter = () => true,
+  sortBy = '-date',
+}: GetProjectsOptions = {}) {
   const paths = import.meta.glob<File>('/src/content/projects/*.md', { eager: true });
+  const sortDirection = sortBy.slice(0, 1) as SortOperator;
+  const sortKey = sortBy.slice(1) as SortKey;
 
   const projects: Project[] = Object.entries(paths)
-    .map(([path, file]) => {
+    .flatMap(([path, file]) => {
       const slug = path.split('/').at(-1)?.replace('.md', '');
 
       if (file?.metadata && slug && !file.metadata.hidden) {
@@ -17,12 +31,27 @@ export async function getProjects() {
           url: path.replace('/src/content/', '').replace(/\.md$/, ''),
         } satisfies Project;
 
-        return project;
+        return [project];
       }
-      return undefined;
+      return [];
     })
-    .filter((project) => project !== undefined)
-    .sort((first, second) => Date.parse(second.date) - Date.parse(first.date));
+    .filter(customFilter)
+    .sort((a, b) => {
+      let ascResult = 0;
+
+      switch (sortKey) {
+        case 'title':
+          ascResult = a.title.localeCompare(b.title);
+          break;
+        case 'date':
+          ascResult = new Date(a.date).getTime() - new Date(b.date).getTime();
+          break;
+        default:
+          throw new Error(`sortBy option not valid: "${sortBy}"`);
+      }
+
+      return sortDirection === '-' ? ascResult * -1 : ascResult;
+    });
 
   return projects;
 }
