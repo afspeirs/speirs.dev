eda2298ddace116e6d00b5cd0e6d1a5a25182b57
Andrew Speirs <andrew@speirs.dev>
2025-10-19 21:17:44 +0100

    feat: add layout e2e tests

---
+++ cypress/e2e/layout.cy.ts
@@ -0,0 +1,39 @@
+describe('Layout', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  describe('Header', () => {
+    it('should display the header', () => {
+      cy.get('header').should('be.visible');
+    });
+
+    it('should display the logo', () => {
+      cy.get('header a[aria-label="Home"]').should('be.visible');
+    });
+  });
+
+  describe('Footer', () => {
+    it('should display the footer', () => {
+      cy.get('footer').should('be.visible');
+    });
+
+    it('should display the copyright notice', () => {
+      cy.get('footer').contains('© 2017-2025 Andrew Speirs');
+    });
+
+    it('should display the theme toggle button', () => {
+      cy.get('footer button').should('be.visible');
+    });
+
+    it('should toggle the theme', () => {
+      cy.get('html').then(($html) => {
+        const initialTheme = $html.attr('data-theme');
+
+        cy.get('footer button').click();
+
+        cy.get('html').should('not.have.attr', 'data-theme', initialTheme);
+      });
+    });
+  });
+});
